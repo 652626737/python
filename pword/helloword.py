5160eca99f9eda605ee6d9fb2eb4c282637e67e1
liyong <652626737@qq.com>
2018-07-05 16:50:48 +0800

    Signed-off-by: liyong <652626737@qq.com>

--- pword/helloword.py
+++ pword/helloword.py
@@ -5,7 +5,6 @@ Created on 2018年7月3日
 @author: liyong
 '''
 #这是第一个注释
-
 print "hello word"
 
 # 这是第二个注释
@@ -52,6 +51,8 @@ print b
 print c
 print d
 
+
+print"-----------------"
 a=30
 b=40
 c= 50
@@ -84,8 +85,45 @@ c=0
 if(a == b):
     print"1-a=b"
 else:
-    print"2-a不等于b"
+    print"1-a不等于b"
 if(a!=b):
-    print"3-a不等于b"
+    print"2-a不等于b"
 else:
-    print "4-a等于b" 
+    print "2-a等于b" 
+if(a<b):
+    print "3-a小于b"
+else:
+    print "3-a不小于b"
+if(a>b):
+    print "4-a大于b"
+else:
+    print"4-a不大于b"
+print"---------------------"
+#修改变量a和b的值
+a=5
+b=20
+if(a<=b):
+    print "5-a小于等于b"
+else:
+    print "a大于b"
+if(b>=a):
+    print "b大于等于a"
+    
+print "---------------------"
+a=60
+b = 13
+print "---------------------"
+c = a & b
+print "a & b=",c
+print "---------------------"
+c=a|b
+print "a|b=",c
+print "---------------------"
+c=a^b
+print "a^b=",c
+print "---------------------"
+
+print "a按位取反为:",~a
+print "a左移运算符",a<<2
+print "b"
+    
