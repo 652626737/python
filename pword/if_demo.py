a08b93f0db211902e60888a35cd990f2b824fb80
652626737@qq.com <liyong@bogon>
2018-07-09 19:54:36 +0800

    Signed-off-by: 652626737@qq.com <liyong@bogon>

--- pword/if_demo.py
+++ pword/if_demo.py
@@ -14,3 +14,15 @@ if name == "python":
     print "welcome boss"
 else:
     print name
+num = 5
+
+if num ==3:
+    print "boss"
+elif num ==2:
+    print "user"
+elif num==1:
+    print "worker"
+elif num<0:
+    print "erro"
+else:
+    print "roadman"
