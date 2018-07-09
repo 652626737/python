6fd268050d221987a9e8f97653d871dd4155b920
652626737@qq.com <liyong@bogon>
2018-07-09 19:41:13 +0800

    Signed-off-by: 652626737@qq.com <liyong@bogon>

---
+++ pword/ifdemo.py
@@ -0,0 +1,16 @@
+#-*-coding=utf-8 -*-
+#!/usr/bin/env python 
+'''
+Created on 2018年7月9日
+
+@author: liyong
+'''
+#if基本语法
+from pword.helloword import name
+flag = False
+name = 'luren'
+if name == "python":
+    flag = True
+    print "welcome boss"
+else:
+    print name
